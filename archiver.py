f3fcbaeeaa27b25dc727046c4fdd43f7b45f8869
sakshamchawla <saksham0@yahoo.in>
2020-03-20 16:28:56 -0400

    Creates a new playlist

--- archiver.py
+++ archiver.py
@@ -2,13 +2,19 @@ import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
+from datetime import date
+
 
 config_file = 'config'
-client_id=''
-client_secret=''
-redirect_uri=''
-username=''
-scope=''
+client_id = ''
+client_secret = ''
+redirect_uri = ''
+username = ''
+scope = ''
+sp = ''
+
+date_before = '2019-09-11T00:00:00Z'
+
 
 def setUp():
     with open(config_file, 'r') as config:
@@ -19,17 +25,35 @@ def setUp():
         redirect_uri = configVals[2]
         scope = configVals[3]
         username = configVals[4]
+    token = util.prompt_for_user_token(
+        username=username, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    global sp
+    sp = spotipy.Spotify(auth=token)
+
 
 def show_tracks(results):
+    tracks_to_move = []
+
     for item in results['items']:
         track = item['track']
-        print("%32.32s %32.32s %32s" % (track['artists'][0]['name'], track['name'], item['added_at']))
+        print("%32.32s %32.32s %32s %s" % (
+            track['artists'][0]['name'], track['name'], item['added_at'], date_before >= item['added_at']))
+        if date_before >= item['added_at']:
+            tracks_to_move.append(track['id'])
+    print(tracks_to_move)
+
+
+def create_playlist():
+    today = date.today().strftime('%b %Y')
+    print(today)
+    if sp:
+        results = sp.user_playlist_create(username, 'Archive ' + str(today), public=True, description='Created by spotify-archive')
+        return results['id']
+    else:
+        return None
 
 def getSavedSongs():
-    token = util.prompt_for_user_token(username=username,scope=scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
+    if sp:
         results = sp.current_user_saved_tracks()
         show_tracks(results)
         while results['next']:
@@ -38,5 +62,7 @@ def getSavedSongs():
     else:
         print("Can't get token for", username)
 
+
 setUp()
 getSavedSongs()
+new_playlist_id = create_playlist()
